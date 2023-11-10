609f253c063b9812489a2ae205dc62f6220ca393
Vladimir Sidortsov <TungFram@yandex.ru>
2023-11-10 17:52:09 +0300

    feat: init commit

---
+++ src/test/java/ru/shtybcompany/ratesaggregator/RatesAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shtybcompany.ratesaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatesAggregatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
